574d0725cf75186cd70753104a8541f9e5e9e18e
Ksheharyar <sheharyar1524@gmail.com>
2026-04-17 22:59:29 +0530

    Add App Router sitemap and robots metadata routes

---
+++ consistency-tracker/apps/web/app/sitemap.js
@@ -0,0 +1,16 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://goaltracker.tech',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://goaltracker.tech/login',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://goaltracker.tech/signup',
+      lastModified: new Date(),
+    },
+  ];
+}
